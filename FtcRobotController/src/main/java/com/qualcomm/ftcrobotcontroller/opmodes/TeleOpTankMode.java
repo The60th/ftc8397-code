863828f07926a5e2b8f6c1a1d19710845b684f91
ftcteam8397 <ftc8397@gmail.com>
2015-11-19 16:39:18 -0500

    Initial commit
    Initial commit

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
@@ -0,0 +1,51 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class TeleOpTankMode  extends OpMode {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    DcMotor Arm1;
+    DcMotor Arm2;
+
+    @Override
+    public void init() {
+        //get references to the motors from the hardware map
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
+        Arm1 =hardwareMap.dcMotor.get("Arm1");
+        Arm2 = hardwareMap.dcMotor.get("Arm2");
+        //reverse the right motor
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        //get the values from the gamepads
+        //note: pushing the stick all the way up returns -1,
+        // so we need to reverse the values
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+        float arm1 = gamepad2.left_stick_y;
+        float arm2 = gamepad2.right_stick_y;
+        //set the power of the motors with the gamepad values
+        leftMotor.setPower(leftY);
+        rightMotor.setPower(rightY);
+        Arm2.setPower(arm2);
+        Arm1.setPower(arm1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
