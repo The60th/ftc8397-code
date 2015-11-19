2431606c49d9bcb4649f2d2d4f42b1a87c1204ef
ftcteam8397 <ftc8397@gmail.com>
2015-11-19 17:57:06 -0500

    Added secend srevo (Bradley)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
@@ -11,11 +11,15 @@ public class TeleOpTankMode  extends OpMode {
     final double Left_Spin = 1.0;
     final double Left_Spin_Stop = 0.5;
 
+    final double Right_Spin = 0.0;
+    final double Right_Spin_Stop = 0.5;
+
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor Arm1;
     DcMotor Arm2;
     Servo   Servo1;
+    Servo   Servo2;
     @Override
     public void init() {
         //get references to the motors from the hardware map
@@ -24,6 +28,7 @@ public class TeleOpTankMode  extends OpMode {
         Arm1 =hardwareMap.dcMotor.get("Arm1");
         Arm2 = hardwareMap.dcMotor.get("Arm2");
         Servo1 = hardwareMap.servo.get("Servo1");
+        Servo2 = hardwareMap.servo.get("Servo2");
         //reverse the right motor
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
@@ -45,9 +50,11 @@ public class TeleOpTankMode  extends OpMode {
         Arm1.setPower(arm1);
         if(gamepad1.x) {
             Servo1.setPosition(Left_Spin);
+            Servo2.setPosition(Right_Spin);
         }
         else {
             Servo1.setPosition(Left_Spin_Stop);
+            Servo2.setPosition(Right_Spin_Stop);
         }
 
 
