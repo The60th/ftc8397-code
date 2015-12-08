606e0a2de1f1371f771f4848a35823d3b7769496
ftcteam8397 <ftc8397@gmail.com>
2015-12-08 17:19:16 -0500

    In meeting updates 12/8/15:  Files: TeleTankMode:   Added the new servo's to the code for this program so they we will have a robot that can pick up block and balls.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
@@ -8,11 +8,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOpTankMode  extends OpMode {
 
-    final double Left_Spin = 1.0;
-    final double Left_Spin_Stop = 0.5;
+    final double Spin = 1.0;
+    final double Spin_Stop = 0.5;
 
-    final double Right_Spin = 0.0;
-    final double Right_Spin_Stop = 0.5;
+    //final double Right_Spin = 0.0;
+    //final double Right_Spin_Stop = 0.5;
+
+    final double Up_Spin = 0.75;
+    final double Down_Spin = 0;
+
+    final double Up_Spin2 = 0.25;
+    final double Down_Spin2 = 1;
+
 
     DcMotor leftMotor;
     DcMotor rightMotor;
@@ -20,6 +27,7 @@ public class TeleOpTankMode  extends OpMode {
     DcMotor Arm2;
     Servo   Servo1;
     Servo   Servo2;
+    Servo Servo3;
     @Override
     public void init() {
         //get references to the motors from the hardware map
@@ -29,6 +37,7 @@ public class TeleOpTankMode  extends OpMode {
         Arm2 = hardwareMap.dcMotor.get("Arm2");
         Servo1 = hardwareMap.servo.get("Servo1");
         Servo2 = hardwareMap.servo.get("Servo2");
+        Servo3 = hardwareMap.servo.get("Servo3");
         //reverse the right motor
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
@@ -49,13 +58,25 @@ public class TeleOpTankMode  extends OpMode {
         Arm2.setPower(arm2/350);
         Arm1.setPower(arm1/350);
         if(gamepad1.x) {
-            Servo1.setPosition(Left_Spin);
-            Servo2.setPosition(Right_Spin);
+            Servo1.setPosition(Spin);
+            //Servo2.setPosition(Right_Spin);
         }
         else {
-            Servo1.setPosition(Left_Spin_Stop);
-            Servo2.setPosition(Right_Spin_Stop);
+            Servo1.setPosition(Spin_Stop);
+           // Servo2.setPosition(Right_Spin_Stop);
         }
+
+        if(gamepad1.y){
+            Servo2.setPosition(Up_Spin);
+            Servo3.setPosition(Up_Spin2);
+
+        }
+        else{
+            Servo2.setPosition(Down_Spin);
+            Servo3.setPosition(Down_Spin2);
+
+        }
+
 
 
 
