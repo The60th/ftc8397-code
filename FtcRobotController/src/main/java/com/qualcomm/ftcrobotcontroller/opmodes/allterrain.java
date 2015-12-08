a28fca7d97bb10bed5d4061c73b504f47853bb3a
ftcteam8397 <ftc8397@gmail.com>
2015-12-08 17:47:59 -0500

    In meeting updates 12/8/15:  Files: allterrain:   we speed drive motors on allterrain tires

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
@@ -36,13 +36,13 @@ public class allterrain extends OpMode{
         double leftY = -gamepad1.left_stick_y * 100;
         double rightY = -gamepad1.right_stick_y * 100;
 
-        leftMotor.setPower(leftY/350);
-        midLeftMotor.setPower(leftY/350);
-        leftRearMotor.setPower(leftY/350);
+        leftMotor.setPower(leftY/275);
+        midLeftMotor.setPower(leftY/275);
+        leftRearMotor.setPower(leftY/275);
 
-        rightMotor.setPower(rightY/350);
-        midRightMotor.setPower(rightY/350);
-        rightRearMotor.setPower(rightY/350);
+        rightMotor.setPower(rightY/275);
+        midRightMotor.setPower(rightY/275);
+        rightRearMotor.setPower(rightY/275);
     }
 }
 
