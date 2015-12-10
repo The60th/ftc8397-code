a8852668a42b6cbc9842a6617fcd805bb40971f7
ftcteam8397 <ftc8397@gmail.com>
2015-12-10 18:21:52 -0500

    In meeting updates 12/10/15: Last commit for meeting: People who worked on programing Harrison,Justin,Bradley,Jason.  Files: TeleOpTankMode   Worked with servos and drive motors to make them run better and not strain.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpTankMode.java
@@ -8,18 +8,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOpTankMode  extends OpMode {
 
-    final double Spin = 1.0;
-    final double Spin_Stop = 0.5;
+    final double Left_Spin = 1.0;
+    final double Left_Spin_Stop = 0.5;
 
-    //final double Right_Spin = 0.0;
-    //final double Right_Spin_Stop = 0.5;
+    final double Right_Spin = 0.0;
+    final double Right_Spin_Stop = 0.5; //new
 
-    final double Up_Spin = 0.75;
-    final double Down_Spin = 0;
+    final double Up_Spin = 1.0;
+    final double Down_Spin = 0.0;
 
-    final double Up_Spin2 = 0.25;
-    final double Down_Spin2 = 1;
+    final double Up_Spin2 = 0.0;
+    final double Down_Spin2 = 1.0;
 
+    boolean Servo_On = false;
 
     DcMotor leftMotor;
     DcMotor rightMotor;
@@ -28,6 +29,7 @@ public class TeleOpTankMode  extends OpMode {
     Servo   Servo1;
     Servo   Servo2;
     Servo Servo3;
+    Servo Servo4;
     @Override
     public void init() {
         //get references to the motors from the hardware map
@@ -38,6 +40,8 @@ public class TeleOpTankMode  extends OpMode {
         Servo1 = hardwareMap.servo.get("Servo1");
         Servo2 = hardwareMap.servo.get("Servo2");
         Servo3 = hardwareMap.servo.get("Servo3");
+        Servo4 = hardwareMap.servo.get("Servo4");
+
         //reverse the right motor
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
@@ -48,7 +52,7 @@ public class TeleOpTankMode  extends OpMode {
         //note: pushing the stick all the way up returns -1,
         // so we need to reverse the values
         float leftY = -gamepad1.left_stick_y * 100;
-        float rightY = -gamepad1.right_stick_y * 100;
+        float rightY = -gamepad1.right_stick_y * 100; //new
         float arm1 = -gamepad2.left_stick_y * 100;
         float arm2 = -gamepad2.right_stick_y * 100;
 
@@ -57,18 +61,18 @@ public class TeleOpTankMode  extends OpMode {
         rightMotor.setPower(rightY/200);
         Arm2.setPower(arm2/350);
         Arm1.setPower(arm1/350);
-        if(gamepad1.x)
+        if(gamepad1.x) //new
         {
-            Servo1.setPosition(Spin);
-            //Servo2.setPosition(Right_Spin);
+            Servo1.setPosition(Left_Spin);
+            Servo4.setPosition(Right_Spin);  //Servo2.setPosition(Right_Spin);
         }
         else
         {
-            Servo1.setPosition(Spin_Stop);
-           // Servo2.setPosition(Right_Spin_Stop);
+            Servo1.setPosition(Left_Spin_Stop);
+            Servo4.setPosition(Right_Spin_Stop);  // Servo2.setPosition(Right_Spin_Stop);
         }
 
-        if(gamepad1.y)
+/*        if(gamepad1.y)
         {
             Servo2.setPosition(Up_Spin);
             Servo3.setPosition(Up_Spin2);
@@ -80,6 +84,27 @@ public class TeleOpTankMode  extends OpMode {
             Servo3.setPosition(Down_Spin2);
 
         }
+*/
+
+        if(gamepad1.y) { //new
+            //If turbomode is on, turboOn should be true, since it's on and we pressed the button
+            //we want to shut the function off. First we set turboValue to the non turbo mode
+            //division constant, then we set turboOn to false to indicate that the mode is off to
+            //the next iteration of the program
+            if(Servo_On) {
+                Servo2.setPosition(Up_Spin);
+                Servo3.setPosition(Up_Spin2);
+                Servo_On = false;
+            }
+            //This is similar to the above, except it does the inverse function
+            else {
+                Servo2.setPosition(Down_Spin);
+                Servo3.setPosition(Down_Spin2);
+                Servo_On = true;
+            }
+        }
+
+
 
 
 
