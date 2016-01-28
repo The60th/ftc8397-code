462aa4ffbb87371ae86d4b76a7224a4a777aff30
ftcteam8397 <ftc8397@gmail.com>
2016-01-28 18:30:26 -0500

    In meeting update by Justin. Worked on file: Allterrain,we added some code to run the motors on the arm we also changed the code for the turnServo to an if, if, else becsuse the if, else, if, else, did not work. The last thing we did was add a new servo called dumpServo so the robot can dump the blocks into the goals on the ramp.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
@@ -3,7 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.util.Range;
 import java.util.UUID;
 //Created by CanAdirondack on 11/25/2015.
 
@@ -13,70 +13,104 @@ public class allterrain extends OpMode{
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor upMiddleMotor;
-    DcMotor threeArmMotor;
-    //DcMotor oneArmMotor;
-    //DcMotor twoArmMotor;
+    //   DcMotor threeArmMotor;
+   // DcMotor oneArmMotor;
+   // DcMotor twoArmMotor;
     Servo turnServo;
+    Servo dumpServo;
     @Override
     public void init() {
         //get references to the motors from the hardware map
         leftMotor = hardwareMap.dcMotor.get("LM1"); //L_M1 was leftMotor
         rightMotor = hardwareMap.dcMotor.get("RM1"); //R_M1 was rightMotor
-        upMiddleMotor = hardwareMap.dcMotor.get("MM1"); //M_M1 was middlemotor
+       upMiddleMotor = hardwareMap.dcMotor.get("MM1"); //M_M1 was middlemotor
         //threeArmMotor = hardwareMap.dcMotor.get("A_M3"); //A_M3 arm motor 3
         leftRearMotor = hardwareMap.dcMotor.get("LM2"); //L_M2 was leftRearMotor
         rightRearMotor = hardwareMap.dcMotor.get("RM2"); //R_M2 was rightRearMotor
-        //oneArmMotor = hardwareMap.dcMotor.get("oneArmMotor");
-        //twoArmMotor = hardwareMap.dcMotor.get("twoArmMotor");
+       // oneArmMotor = hardwareMap.dcMotor.get("AM1");
+       // twoArmMotor = hardwareMap.dcMotor.get("AM2");
         turnServo = hardwareMap.servo.get("TS1");
+        dumpServo = hardwareMap.servo.get("DS");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
         rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
     }
     @Override
     public void loop() {
-        //Updates values geting rid of the old *100 and /275 and working with just /75
-        double leftY = -gamepad1.left_stick_y;
-        double rightY = -gamepad1.right_stick_y;
-        leftMotor.setPower(rightY/75);
-        //midLeftMotor.setPower(leftY/75);
-        leftRearMotor.setPower(rightY/75);
-        rightMotor.setPower(leftY/75);
-       // midRightMotor.setPower(rightY/75);
-        rightRearMotor.setPower(leftY/75);
-        double leftx = -gamepad2.left_stick_y;
-        double rightx = -gamepad2.right_stick_y;
-        double UpPower = -.50;
+        double leftY = gamepad1.left_stick_y/2;
+        double rightY = gamepad1.right_stick_y/2;
+        leftY = Range.clip(leftY, -1, 1);
+        rightY = Range.clip(rightY, -1,1);
+
+            leftMotor.setPower(leftY);
+            leftRearMotor.setPower(leftY);
+
+            rightMotor.setPower(rightY);
+            rightRearMotor.setPower(rightY);
+
+        //double leftx = -gamepad2.left_stick_y;
+       // double rightx = -gamepad2.right_stick_y;
+       // leftx = Range.clip(leftx, -1,1);
+        //rightx = Range.clip(rightx, -1,1);
+
+       // oneArmMotor.setPower(leftx);
+        //twoArmMotor.setPower(rightx);
+
+       // double leftx = -gamepad2.left_stick_y;
+       // double rightx = -gamepad2.right_stick_y;
+       // double UpPower = -.50;
         //oneArmMotor.setPower(rightx/275);
         //twoArmMotor.setPower(leftx/275);
 
-        if (gamepad2.right_bumper)
-        {
-          //  threeArmMotor.setPower(.5);
-        }
-        else
-        {
-           // threeArmMotor.setPower(0.0);
-        }
+       // if (gamepad2.right_bumper)
+       // {
+       //   //  threeArmMotor.setPower(.5);
+       // }
+       // else
+       // {
+       //    // threeArmMotor.setPower(0.0);
+       // }
+
+
         if (gamepad1.left_bumper)
         {
-            upMiddleMotor.setPower(.25);
+            upMiddleMotor.setPower(-.25);
             if (gamepad1.right_bumper)
             {
-                //upMiddleMotor.setPower(-.25);
+                upMiddleMotor.setPower(.25);
             }
         }
         else
         {
-           // upMiddleMotor.setPower(0.0);
+            upMiddleMotor.setPower(0.0);
         }
+
+
 
         if(gamepad2.left_bumper)
         {
             turnServo.setPosition(0);
+            if(gamepad2.right_bumper)
+            {
+                turnServo.setPosition(1);
+            }
+
+
         }
-        else{
+        else
+        {
           turnServo.setPosition(.5);
         }
+
+        if(gamepad2.x){
+         dumpServo.setPosition(0);
+        }
+        else{
+            dumpServo.setPosition(1);
+        }
+
+
+
+
         //To Do:
         //Work with switching turnservo over to a joystick control rather then with buttons to make it flow better.
         //Also switch over the main arms over to joystick control and trigersr rather then what the are currently set to.
