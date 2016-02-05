6edae0ba044567b19511b1360b8e3e13f3a95044
ftcteam8397 <ftc8397@gmail.com>
2016-02-04 19:02:06 -0500

    Meeting update done by Justin:
     Added ColorTestCopy:  This is just a copy of the main ColorTest program to be rewrote to use as a template for auto programs.
     Added ColorTestCopy2: This is just a copy of the main ColorTest program to be rewrote to use as a template for auto programs.
     Added ColorTestCopy3: This is just a copy of the main ColorTest program to be rewrote to use as a template for auto programs.
    
     Worked on AllTerrain: Updated all robot commands to make the robot run better and cleaner. Also update the contros to flow better and make more sense.
    
     Worked on FtcOpModeRegister: Removed some programs from here to make it cleaner and easyer on the eyes for running on the phones.
    
     Other Patch notes: Highly importan merged this program with the down loaded program from github to update this version, of the app to 1.5 rather then 1.25.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,13 +55,13 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
+    //manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register("K9Line", K9Line.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register("K9Line", K9Line.class);
+    ///manager.register ("PushBotAuto", PushBotAuto.class);
+    //////manager.register ("PushBotManual", PushBotManual.class);
     manager.register("allterrain", allterrain.class);
     manager.register("ColorTest",ColorTest.class);
 
@@ -69,7 +69,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
-    manager.register("MR Gyro Test", MRGyroTest.class);
+   // manager.register("MR Gyro Test", MRGyroTest.class);
 
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
