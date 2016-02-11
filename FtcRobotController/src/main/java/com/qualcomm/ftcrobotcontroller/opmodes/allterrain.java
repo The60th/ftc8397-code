c87d051d40c9de06a6e88321c99aa824c19dcfbe
ftcteam8397 <ftc8397@gmail.com>
2016-02-11 18:09:58 -0500

    Meeting update done by Justin:
    Adding some small code for some better turn control to make the robot turn faster by drive at the same speed. Not yet tested shall be tested over the weekend.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/allterrain.java
@@ -38,23 +38,35 @@ public class allterrain extends OpMode
 
     }
     @Override
+    //Notice most comments are out of date currently please ignore.
     public void loop()
     {
-        double leftY = gamepad1.left_stick_y/2.5; //2am
+        double leftY = gamepad1.left_stick_y/2.5; //2am || 0.4 when joystick at max power
         double rightY = -gamepad1.right_stick_y/2.5;// am
-
+        double JoyOneLeft = gamepad1.left_stick_y/1.25;
+        double JoyOneRight = -gamepad1.right_stick_y/1.25;
         double rightX = -gamepad2.left_stick_y/2;  //is driving 1AM          // this power is going to MM1 and rightmotor  RM
         double leftX = -gamepad2.right_stick_y/2;  //is driving MM1         // ^^                                       MM1
-
         //MM1 and armmotor are comeing in as left and right
          //Left right motor are showing up as two and three for some reason
-
-
         leftMotor.setPower(leftX); //runs arm motors as leftmotor
         rightMotor.setPower(rightX); //runs arm motors as right motor
+        //Current drive wheel set up.
+        //running as MM1
+        if(leftY == 1 || rightY == -1){
+            leftY = 0;
+            rightY = 0;
+            oneArmMotor.setPower(JoyOneRight);
+            upMiddleMotor.setPower(JoyOneLeft);
+        }
+        else
+        {
+            oneArmMotor.setPower(rightY); //runing as 1AM, but on right wheels?
+            upMiddleMotor.setPower(leftY);
 
-        oneArmMotor.setPower(rightY); //runing as 1AM, but on right wheels?
-        upMiddleMotor.setPower(leftY); //running as MM1
+
+        }
+
 
 
       //***********************************
@@ -115,31 +127,31 @@ public class allterrain extends OpMode
         }
         //Full set of debug info.
         telemetry.addData("Turn Servo's current position is:",turnServo.getPosition());
-        telemetry.addData("Dump Servo's current position is:",dumpServo.getPosition());
+        telemetry.addData("Dump Servo's current position is:",dumpServo.getPosition());/*
         telemetry.addData("Left Motor's assigned name is:",leftMotor.getDeviceName());
         telemetry.addData("Left Motor's current connection info is :", leftMotor.getConnectionInfo());
-        telemetry.addData("left motor power:",leftMotor.getPower());
+        */telemetry.addData("left motor power:",leftMotor.getPower());/*
         telemetry.addData("MM1 name:",upMiddleMotor.getDeviceName());
         telemetry.addData("MM1: connect info",upMiddleMotor.getConnectionInfo());
-        telemetry.addData("MM1 power:",upMiddleMotor.getPower());
+        */telemetry.addData("MM1 power:",upMiddleMotor.getPower());/*
         telemetry.addData("Right Motor's assigned name is:", rightMotor.getDeviceName());
         telemetry.addData("Right Motor's current connection info is :", rightMotor.getConnectionInfo());
-        telemetry.addData("right motor power:",rightMotor.getPower());
+        */telemetry.addData("right motor power:",rightMotor.getPower());/*
         telemetry.addData("1AM assigned name is:",oneArmMotor.getDeviceName());
         telemetry.addData("1AM current connection info is :", oneArmMotor.getConnectionInfo());
-        telemetry.addData("1AM power:", oneArmMotor.getPower());
+       */ telemetry.addData("1AM power:", oneArmMotor.getPower());/*
         telemetry.addData("2AM assigned name is:",twoArmMotor.getDeviceName());
         telemetry.addData("2AM current connection info is :", twoArmMotor.getConnectionInfo());
-        telemetry.addData("2AM power:", twoArmMotor.getPower());
+       */ telemetry.addData("2AM power:", twoArmMotor.getPower());/*
         telemetry.addData("3AM assigned name is:",threeArmMotor.getDeviceName());
         telemetry.addData("3AM current connection info is :", threeArmMotor.getConnectionInfo());
-        telemetry.addData("3AM:", threeArmMotor.getPower());
+       */ telemetry.addData("3AM:", threeArmMotor.getPower());/*
         telemetry.addData("TS assigned name is:",turnServo.getDeviceName());
         telemetry.addData("TS current connection info is :", turnServo.getConnectionInfo());
         telemetry.addData("DS assigned name is:",dumpServo.getDeviceName());
-        telemetry.addData("DS current connection info is :", dumpServo.getConnectionInfo());
+        telemetry.addData("DS current connection info is :", dumpServo.getConnectionInfo()); */
 
-
+//// TODO: 2/11/2016 added a speed up for the turn, idea on how it works is: When a both joysticks are on full max values as in 1,-1. It would increase the value from a /2 to a /1.25.
         //To Do:
         //Work with switching turnservo over to a joystick control rather then with buttons to make it flow better.
         //Also switch over the main arms over to joystick control and trigersr rather then what the are currently set to.
