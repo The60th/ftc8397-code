b8879e758778b97c0b7050c4cf531a9e72f429c4
ftcteam8397 <ftc8397@gmail.com>
2016-09-30 21:28:39 -0400

    9/30/16 This program runs are robot that has wheels at 45 degree angles.  Right now it does not turn but that is what we are going to work on next.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test45.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+import android.widget.Switch;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.ftccommon.DbgLog;
+
+
+/**
+ * Created by CanAdirondack on 9/30/2016.
+ */
+@TeleOp(name=" Test45 : Test ", group="Test")
+
+public class test45  extends OpMode {
+
+    DcMotor one;
+    DcMotor two;
+    DcMotor three;
+    DcMotor four;
+
+    @Override
+    public void init() {
+
+        one = hardwareMap.dcMotor.get("M1");
+        two = hardwareMap.dcMotor.get("M2");
+        three = hardwareMap.dcMotor.get("M3");
+        four = hardwareMap.dcMotor.get("M4");
+
+        two.setDirection(DcMotor.Direction.REVERSE);
+        three.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+    public void loop(){
+
+        if (gamepad1.right_stick_y <= -.05) {
+        //Forward drive to the north east.
+            one.setPower(1);
+            three.setPower(1);
+        }
+
+        else if (gamepad1.right_stick_y >= .05) {
+        //Backwards drive to the south west.
+            one.setPower(-1);
+            three.setPower(-1);
+            }
+        else {
+            one.setPower(0);
+            three.setPower(0);
+        }
+
+
+
+            if (gamepad1.right_stick_x <= -.05) {
+                two.setPower(1);
+                four.setPower(1);
+            }
+            else if (gamepad1.right_stick_x >= .05) {
+
+            two.setPower(-1);
+            four.setPower(-1);
+        }
+            else{
+            two.setPower(0);
+            four.setPower(0);
+        }
+
+
+    }
+
+
+
+    }
+
+
