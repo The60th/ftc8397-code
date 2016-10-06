62c98299875a09391cdd33ff3f495f924b858f18
ftcteam8397 <ftc8397@gmail.com>
2016-10-06 15:49:51 -0400

    9/30/16 This program runs a robot at any angle and lets it turn without changing potions.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+import android.widget.Switch;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.ftccommon.DbgLog;
+
+
+/**
+ * Created by CanAdirondack on 9/30/2016.
+ */
+@TeleOp(name=" Lift : Test ", group="Test")
+
+public class test45  extends OpMode {
+
+
+
+    @Override
+    public void init() {
