fa600f1f2d369489ef1cb0dcda6624d680555ce9
ftcteam8397 <ftc8397@gmail.com>
2016-10-27 16:53:03 -0400

    10/27/16. Work in progress.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test45.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test45.java
@@ -29,7 +29,7 @@ public class test45  extends LinearOpMode {
     DcMotor RightLaunch;
 
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         one = hardwareMap.dcMotor.get("M1");
         two = hardwareMap.dcMotor.get("M2");
         three = hardwareMap.dcMotor.get("M3");
@@ -40,53 +40,57 @@ public class test45  extends LinearOpMode {
         LeftLaunch = hardwareMap.dcMotor.get("LL");
         RightLaunch = hardwareMap.dcMotor.get("RL");
 
+        LeftLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        RightLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         RightLaunch.setDirection(DcMotor.Direction.REVERSE);
         four.setDirection(DcMotor.Direction.REVERSE);
         three.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
-
+        while (true){
         double F_B_Drive = gamepad1.right_stick_y;
         double L_R_Drive = gamepad1.right_stick_x;
         double TurnDrive = gamepad1.left_stick_x;
         boolean robotIsStopped = Math.abs(F_B_Drive) < .05 && Math.abs(L_R_Drive) < .05;
 
         boolean changeMode = (turnMode && Math.abs(TurnDrive) < .05 && !robotIsStopped) || (!turnMode && Math.abs(TurnDrive) > .05 && robotIsStopped);
-        if(changeMode){
+        if (changeMode) {
             turnMode = !turnMode;
         }
-        if(turnMode){
+        if (turnMode) {
             moveAtTurnMode();
-        }
-        else{
+        } else {
             moveAtJoystickAngle();
         }
 
-        if (gamepad1.left_trigger >= .5){
+        if (gamepad1.left_trigger >= .5) {
 
             Lift.setPower(1);
 
-        }
-        else if(gamepad1.right_trigger >= .5){
+        } else if (gamepad1.right_trigger >= .5) {
 
             Grabber.setPower(-1);
-        }
-        else {
+        } else {
             Lift.setPower(0);
             Grabber.setPower(0);
         }
-        if (gamepad1.left_bumper){
+        if (gamepad1.left_bumper) {
             LeftLaunch.setPower(-1);
             RightLaunch.setPower(-1);
+        } else {
+            double x = 1;
+            for (double i = 1; i > .01; i = i - .25)
+                x = x - .25;
+            //i = i;
+            LeftLaunch.setPower(x);
+            RightLaunch.setPower(x);
+            //wait(250);
         }
-        else{
-            for(double i = 1; i>.01; i = i-.25)
-            LeftLaunch.setPower(0);
-            RightLaunch.setPower(0);
-            wait(250);
-
-        }
+        LeftLaunch.setPower(0);
+        RightLaunch.setPower(0);
     }
 
+}
 
     public double moveAtJoystickAngle(){
 
@@ -137,19 +141,19 @@ public class test45  extends LinearOpMode {
             four.setPower(0);
             return;
         }
-        if (turnDrive >= .05 ){
+        if (turnDrive >= .05 ){ //Is right on the joystick and spins the robot right
             one.setPower(.5);
-            two.setPower(-.5);
+            two.setPower(.5);
             three.setPower(-.5);
-            four.setPower(.5);
+            four.setPower(-.5);
             telemetry.addData("Running in turn mode:","");
             this.telemetry.update();
         }
-        else if (turnDrive <= -.05){
+        else if (turnDrive <= -.05){//Is left on the joystick and spins the robot left
             one.setPower(-.5);
-            two.setPower(.5);
+            two.setPower(-.5);
             three.setPower(.5);
-            four.setPower(-.5);
+            four.setPower(.5);
             telemetry.addData("Running in turn mode:","");
             this.telemetry.update();
         }
