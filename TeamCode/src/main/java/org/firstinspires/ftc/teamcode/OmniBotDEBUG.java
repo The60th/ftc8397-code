de5396df71f5e148a89f620c34761826f462ea46
ftcteam8397 <ftc8397@gmail.com>
2016-12-15 21:26:23 -0500

    12/15/16
    Added debug programs(OmniBotDEBUG and OmniBotDebugOpMode) both contain debug data for motor encoders.
    Added AutonmousRedSide and AutonmousBlueSide both are copys of AutonomousOpMode1 other then the fact they lack code for the gyro sensor(Which was currently not working) and redSide has been mirrored to run a correct code path for a red side Autonomous period.
    Added support to OmniBot for gyro sensors, a gyro will now be needed in your config files to run the OmniBot class.
    Inside OmniBot we added support functions for gyro sensor inside here.
    Updated OmniBot with functions to handle robot x,y,z plane cordinate tracking and driving.
    Functions include ability to use the motor encoders and gyro sensor to tracking robot pos.
    
    OmniBot now supports gyro sensor and encoders better.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniBotDEBUG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniBotDEBUG.java
@@ -1,43 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.matrices.GeneralMatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-public class OmniBot
+public class OmniBotDEBUG
 {
     //Robot constants
-    public final  float ONE_ROOT_TWO = (float)(1/Math.sqrt(2));
-    public final double TICKS_PER_MOTOR_ROTATION = 1120.0; // With a 1 to 40 gearbox.
+    public final double TICKS_PER_MOTOR_ROTATION = 1120; // With a 1 to 40 gearbox.
     public final double GEAR_RATIO = 1.0;  //Motor rotations per wheel rotation.
     public final double WHEEL_DIAMETER = 4.0*2.54;  //4in converted to cm.
     public final double TICKS_PER_CM = TICKS_PER_MOTOR_ROTATION*GEAR_RATIO/(Math.PI*WHEEL_DIAMETER);
     public final double LENGTH = 36.8; //cm
     public final double WIDTH = 30.5;  //cm
     public final double TURN_RADIUS = .5*(Math.sqrt(LENGTH*LENGTH+WIDTH*WIDTH));
-    public final float COS_BETA = (float)( (WIDTH+LENGTH)/(2.0*Math.sqrt(2.0)*TURN_RADIUS));
-    public final float R_COS_BETA = (float)(TURN_RADIUS*COS_BETA);
-    public final double MIN_RED = 20.0;
-    public final double MIN_BLUE = 20.0;
-    public final double MAX_SENSOR_VALUES = 255.0;
-    public final GeneralMatrixF ROBOT_WHEEL_TRANSFORM = new GeneralMatrixF(4,4,
-            new float[] {-ONE_ROOT_TWO, ONE_ROOT_TWO,-R_COS_BETA,1
-                    , ONE_ROOT_TWO, ONE_ROOT_TWO,-R_COS_BETA,-1
-                    ,-ONE_ROOT_TWO, ONE_ROOT_TWO,R_COS_BETA,-1
-                    , ONE_ROOT_TWO, ONE_ROOT_TWO,R_COS_BETA,1} );
-    public final MatrixF WHEEL_ROBOT_TRANSFORM = ROBOT_WHEEL_TRANSFORM.inverted();
-    public VectorF last_Wheel_Ticks = new VectorF(0,0,0,0);
-    public float last_Gyro_Theta = 0;
-
-
+    public final double COS_BETA = (WIDTH+LENGTH)/(2*Math.sqrt(2)*TURN_RADIUS);
+    public final double MIN_RED = 20;
+    public final double MIN_BLUE = 20;
+    public final double MAX_SENSOR_VALUES = 255;
 
     /* Public OpMode members. */
     public DcMotor one;
@@ -60,7 +47,7 @@ public class OmniBot
 
 
     /* Constructor */
-    public OmniBot(){
+    public OmniBotDEBUG(){
 
     }
 
@@ -89,9 +76,9 @@ public class OmniBot
         RightLaunch = hardwareMap.dcMotor.get("RL");
 
         sensorGyro.setHeadingMode(ModernRoboticsI2cGyro.HeadingMode.HEADING_CARTESIAN);
-
         LeftLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         RightLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         RightLaunch.setDirection(DcMotor.Direction.REVERSE);
         four.setDirection(DcMotor.Direction.REVERSE);
         three.setDirection(DcMotor.Direction.REVERSE);
@@ -120,6 +107,10 @@ public class OmniBot
         w2 = w2/max;
         w3 = w3/max;
         w4 = w4/max;
+
+        //Debug output
+        DbgLog.msg("Requested Wheel Powers: %.2f %.2f %.2f %.2f", w1, w2, w3, w4);
+
         one.setPower(w1);
         two.setPower(w2);
         three.setPower(w3);
@@ -146,6 +137,9 @@ public class OmniBot
     }
 
     public void setMaxDriveTicksPerSec(int TicksPerSec){
+
+        //Debug Output
+        DbgLog.msg("Requested Max Drive Ticks/Sec: %d", TicksPerSec);
 
         one.setMaxSpeed(TicksPerSec);
         two.setMaxSpeed(TicksPerSec);
@@ -178,58 +172,6 @@ public class OmniBot
     public float[] getColorValues(){
         float[] colorValues = {sensorRGB_One.red(),sensorRGB_One.green(),sensorRGB_One.blue()};
        return colorValues;
-    }
-
-    public void updateOdometry(){
-        last_Wheel_Ticks.put(0,one.getCurrentPosition());
-        last_Wheel_Ticks.put(1,two.getCurrentPosition());
-        last_Wheel_Ticks.put(2,three.getCurrentPosition());
-        last_Wheel_Ticks.put(3,four.getCurrentPosition());
-        last_Gyro_Theta = (float)((double)-sensorGyro.getIntegratedZValue()*(Math.PI/180.0));
-
-    }
-
-    public float[] updateOdometry(float x, float y, float theta){
-        VectorF curTicks = new VectorF(one.getCurrentPosition(),two.getCurrentPosition(),three.getCurrentPosition(),four.getCurrentPosition());
-        VectorF newTicks = curTicks.subtracted(last_Wheel_Ticks);
-        VectorF deltaWheelCM = newTicks.multiplied((float)(1.0f/TICKS_PER_CM));
-
-        VectorF deltaRobotPos = WHEEL_ROBOT_TRANSFORM.multiplied(deltaWheelCM);
-        final float sin = (float)(Math.sin(theta+deltaRobotPos.get(2)/2.0));
-        final float cosin = (float)(Math.cos(theta+deltaRobotPos.get(2)/2.0));
-
-        float newX = x+deltaRobotPos.get(0)*sin +deltaRobotPos.get(1)*cosin;
-        float newY = y+deltaRobotPos.get(1)*sin - deltaRobotPos.get(0)*cosin;
-        float newTheta = theta+deltaRobotPos.get(2);
-        last_Wheel_Ticks = newTicks;
-        last_Gyro_Theta = (float)((double)-sensorGyro.getIntegratedZValue()*(Math.PI/180.0));
-        return new float[]{newX,newY,newTheta};
-    }
-    public float[] updateOdometry(float x,float y,float theta,float newThetaGyro){
-
-        return null;
-    }
-    public float[] updateOdometryGyro(float x,float y,float theta){
-        VectorF curTicks = new VectorF(one.getCurrentPosition(),two.getCurrentPosition(),three.getCurrentPosition(),four.getCurrentPosition());
-        VectorF newTicks = curTicks.subtracted(last_Wheel_Ticks);
-        VectorF deltaWheelCM = newTicks.multiplied((float)(1.0f/TICKS_PER_CM));
-
-        VectorF deltaRobotPos = WHEEL_ROBOT_TRANSFORM.multiplied(deltaWheelCM);
-        float newGyroTheta = (float)((double)-sensorGyro.getIntegratedZValue()*(Math.PI/180.0));
-        float deltaGyroTheta = newGyroTheta-last_Gyro_Theta;
-
-        final float sin = (float)(Math.sin(theta+deltaGyroTheta/2.0));
-        final float cosin = (float)(Math.cos(theta+deltaGyroTheta/2.0));
-
-        float newX = x+deltaRobotPos.get(0)*sin +deltaRobotPos.get(1)*cosin;
-        float newY = y+deltaRobotPos.get(1)*sin - deltaRobotPos.get(0)*cosin;
-        float newTheta = theta+newGyroTheta;
-        last_Wheel_Ticks = newTicks;
-        last_Gyro_Theta = newGyroTheta;
-
-        return new float[]{newX,newY,newTheta};
-
-
     }
 
 
